4fd6fda8d069945593ffa8097274913bc0fad294
Ayush Bherwani <ayush.bherwani1998@gmail.com>
2023-08-03 14:22:06 +0530

    adds Image grid view

--- test/features/home/presentation/pages/home_page_test.dart
+++ test/features/home/presentation/pages/home_page_test.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:unplash_sample/core/utils/string_constants.dart';
 import 'package:unplash_sample/core/widgets/error_tile.dart';
+import 'package:unplash_sample/features/home/data/models/image_model.dart';
 import 'package:unplash_sample/features/home/presentation/bloc/unsplash_image_bloc.dart';
 import 'package:unplash_sample/features/home/presentation/pages/home_page.dart';
+import 'package:unplash_sample/features/home/presentation/widgets/image_gridview_widget.dart';
 
 import '../../../../core/mocks/unsplash_image_bloc_mock.dart';
+import '../../../../fixtures/fixture_reader.dart';
 import '../../../../mock_dependency_injection.dart';
 
 void main() {
@@ -77,6 +82,30 @@ void main() {
       final progressIndicator = find.byType(CircularProgressIndicator);
 
       expect(progressIndicator, findsOneWidget);
+    });
+
+    testWidgets("should show ImageGridViewWidget on UnsplashImageLoadedState",
+        (WidgetTester tester) async {
+      final unsplashImageListModel = UnsplashImageListModel.fromJson(
+        List<Map<String, dynamic>>.from(
+            jsonDecode(fixture('image_model_fixture.json'))),
+      );
+
+      when(() => bloc.stream).thenAnswer((_) {
+        return Stream.value(
+          UnsplashImageLoadedState(unsplashImageListModel.images),
+        );
+      });
+
+      when(() => bloc.state).thenReturn(
+        UnsplashImageLoadedState(unsplashImageListModel.images),
+      );
+
+      await tester.pumpWidget(pumpMaterialApp(const HomePage()));
+
+      final imageGridView = find.byType(ImageGridViewWidget);
+
+      expect(imageGridView, findsOneWidget);
     });
   });
 }
