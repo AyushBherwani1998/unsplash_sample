d03659713d4455d5d515ca9fe6a3af69af708119
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-08-03 12:53:15 +0530

    add ontap test

--- test/core/widgets/error_tile_test.dart
+++ test/core/widgets/error_tile_test.dart
@@ -3,20 +3,38 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unplash_sample/core/widgets/error_tile.dart';
 
 void main() {
+  const text = "test";
+  final List<String> logs = [];
+
+  Widget pumpErrorTileWidget() {
+    return MaterialApp(
+      home: Scaffold(
+          body: ErrorTile(
+        onTap: () {
+          logs.add(text);
+        },
+        message: text,
+      )),
+    );
+  }
 
   group("ErrorTile tests", () {
-    const text = "test";
     testWidgets("text is displayed on screen", (tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: ErrorTile(onTap: () {}, message: text),
-          ),
-        ),
-      );
+      await tester.pumpWidget(pumpErrorTileWidget());
 
       final errorText = find.text(text);
       expect(errorText, findsOneWidget);
+    });
+
+    testWidgets("onTap is called when tile is pressed", (tester) async {
+      await tester.pumpWidget(pumpErrorTileWidget());
+
+      expect(logs.isEmpty, isTrue);
+
+      await tester.tap(find.byType(ErrorTile));
+      await tester.pumpAndSettle();
+
+      expect(logs.isEmpty, isFalse);
     });
   });
 }
