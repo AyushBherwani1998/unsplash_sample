1036a7e8236542527555f33b4c467dc824facbc2
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-08-13 10:21:33 +0530

    add variant support for share button

--- lib/core/config/unleash_config.dart
+++ lib/core/config/unleash_config.dart
@@ -1,9 +1,14 @@
 import 'package:unleash_proxy_client_flutter/unleash_proxy_client_flutter.dart';
 
 const String isImageDetailsEnabledToggleKey = "isImageDetailsEnabled";
+const String shareOptionExperimentKey = "shareOptionExperiment";
+
+enum ShareButtonPosition { gridTile, imageDetails }
 
 abstract class UnleashConfig {
   bool get isDetailsPageEnabled;
+  bool get isShareOptionExperimentEnabled;
+  ShareButtonPosition get shareButtonPosition;
 }
 
 class UnleashConfigImpl extends UnleashConfig {
@@ -14,4 +19,14 @@ class UnleashConfigImpl extends UnleashConfig {
   @override
   bool get isDetailsPageEnabled =>
       unleash.isEnabled(isImageDetailsEnabledToggleKey);
+
+  @override
+  bool get isShareOptionExperimentEnabled =>
+      unleash.isEnabled(shareOptionExperimentKey);
+
+  @override
+  ShareButtonPosition get shareButtonPosition {
+    final variant = unleash.getVariant(shareOptionExperimentKey);
+    return ShareButtonPosition.values.byName(variant.name);
+  }
 }
