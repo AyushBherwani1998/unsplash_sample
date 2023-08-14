8101f7afd0e15ba5579457550f361c989745ee9f
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-08-14 22:58:22 +0530

    remove unused import

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:unplash_sample/core/widgets/error_tile.dart';
