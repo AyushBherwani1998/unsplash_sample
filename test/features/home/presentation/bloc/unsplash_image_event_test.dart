ba2d5f96993ec54a02809feb63909236195f63fa
Ayush Bherwani <ayush.bherwani1998@gmail.com>
2023-08-16 09:33:55 +0530

    improve coverage

---
+++ test/features/home/presentation/bloc/unsplash_image_event_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unplash_sample/features/home/domain/usecases/fetch_images.dart';
+import 'package:unplash_sample/features/home/presentation/bloc/unsplash_image_bloc.dart';
+
+void main() {
+  test('Validate FetchImageEvent equality', () {
+    final params = FetchImageParams(1, 10);
+    final event = FetchImageEvent(params);
+
+    expect(event.props, [params.page, params.perPage]);
+    expect(event, FetchImageEvent(params));
+  });
+}
---
+++ test/features/image_details/presentation/bloc/image_details_event_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unplash_sample/features/image_details/presentation/bloc/image_details_bloc.dart';
+
+void main() {
+  test('Validate FetchImageEvent equality', () {
+    const id = "id";
+    const event = FetchImageDetailsEvent(id);
+
+    expect(event.props, [id]);
+    expect(event, const FetchImageDetailsEvent(id));
+  });
+}
