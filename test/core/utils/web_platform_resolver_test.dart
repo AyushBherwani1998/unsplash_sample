00c3e1220ff9f08627a7e1952f302df92f6fee24
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-08-17 12:02:00 +0530

    fix web platform resolver test

---
+++ test/core/utils/web_platform_resolver_test.dart
@@ -0,0 +1,11 @@
+@TestOn("browser")
+
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unplash_sample/core/utils/target_platform_extended.dart';
+
+void main() {
+  test('WebPlatformResolver.isWeb returns true on browser', () {
+    expect(WebPlatformResolverImpl().isWeb, isTrue);
+  });
+}
